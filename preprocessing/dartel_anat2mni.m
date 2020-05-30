df4fedde9cfe0b8dd40a5afa97803bc9a2c5fa06
Yun Tian <sherfly.t@gmail.com>
2020-05-30 10:41:12 +0800

    Create dartel_anat2mni.m

---
+++ preprocessing/dartel_anat2mni.m
@@ -0,0 +1,33 @@
+%% initial spm
+clc;close;clear;
+direc = 'F:\fMRI1500\Niftis';
+spm('Defaults','fMRI');
+spm_jobman('initcfg');
+% clear matlabbatch
+%% prepare matlabbatch
+fprintf('%-40s:', 'Preparing spm batch...');
+Template = cellstr(spm_select('FPListRec', direc, '^Template_6.nii$')); 
+flowfields = cellstr(spm_select('FPListRec', direc, '^u_rc120.*t1_mprage.*\.nii$')); 
+%  获取最终模板图像和所有原始变形场图像
+gmfiles = cellstr(spm_select('FPListRec', direc, '^c120.*t1_mprage.*\.nii$'));
+wmfiles = cellstr(spm_select('FPListRec', direc, '^c220.*t1_mprage.*\.nii$'));
+csffiles = cellstr(spm_select('FPListRec', direc, '^c320.*t1_mprage.*\.nii$'));
+% 获取所有灰质/白质/脑脊液解剖图像
+
+%% create batch
+matlabbatch{1}.spm.tools.dartel.mni_norm.template = {'C:\Users\sherf\Desktop\anat\Template_6.nii'};
+matlabbatch{1}.spm.tools.dartel.mni_norm.data.subjs.flowfields = flowfields;
+matlabbatch{1}.spm.tools.dartel.mni_norm.data.subjs.images = {
+                                                                  gmfiles;
+                                                                  wmfiles;
+                                                                  csffiles
+                                                                  };
+matlabbatch{1}.spm.tools.dartel.mni_norm.vox = [NaN NaN NaN];
+matlabbatch{1}.spm.tools.dartel.mni_norm.bb = [NaN NaN NaN
+                                                   NaN NaN NaN];
+matlabbatch{1}.spm.tools.dartel.mni_norm.preserve = 1; % for VBM: Preserve Amount
+matlabbatch{1}.spm.tools.dartel.mni_norm.fwhm = [6 6 6];
+
+
+%% excute batch
+tic; out = spm_jobman('run',matlabbatch); toc;
