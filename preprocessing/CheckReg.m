0bcf6f1e51a8a83e605cebd88f7d3acfd52a83e8
Yun Tian <sherfly.t@gmail.com>
2020-07-17 19:36:00 +0800

    Update CheckReg.m

--- preprocessing/CheckReg.m
+++ preprocessing/CheckReg.m
@@ -9,10 +9,10 @@ spm_jobman('initcfg');
 %% prepare matlabbatch
 fprintf('%-40s:', 'Preparing spm batch...');
 meanrestfiles = cellstr(spm_select('ExtFPListRec', direc, '^meanua.*sms_bold_2mm.*\.nii$')); 
-%  获取时间层校正场图校正以及头动校正后的数据的平均图像
+%  Obtain the average image of the data
 
 gmfiles = cellstr(spm_select('ExtFPListRec', direc, '^c120.*t1_mprage.*\.nii$'));
-% 获取所有解剖图像
+%  Obtain all gray matter images
 
 %% create batch
 for i = 1:numel(anatfiles)
