a3acf04b7c1ec5cd6b29411bac6c1c8c254372a0
Yun Tian <sherfly.t@gmail.com>
2021-01-04 22:42:06 -0800

    Create unzip_niigz.m

---
+++ preprocessing/unzip_niigz.m
@@ -0,0 +1,10 @@
+% Initialise SPM
+clc;close;clear;
+direc = 'F:\fMRI1500\Niftis';
+spm('Defaults','fMRI');
+spm_jobman('initcfg');
+
+%% unzip all rest nii.gz files
+niigzs = spm_select('FPListRec', direc, '.*\.nii\.gz$'); % obtain images path
+niigzs = cellstr(niigzs);
+tic;cellfun(@gunzip, niigzs);toc;
