81eca1199203dbdfbae1346e1ee0c86f8d4d504e
Yun Tian <sherfly.t@gmail.com>
2021-02-24 11:45:51 +0800

    Update Niftis_Backup.m

--- postprocessing/Niftis_Backup.m
+++ postprocessing/Niftis_Backup.m
@@ -24,7 +24,8 @@ for i = 1:numel(sublist)
 %     destination = [todir, '\Preprocessed_T1\', subid, '\'];
 %     mkdir(destination);
 %     source = fullfile(prepedT1s(i,1).folder, prepedT1s(i,1).name);
-%     copyfile(source, destination);
+%     gzip(source, destination);
+%     % copyfile(source, destination);
 %     
 %     % copy raw Rest niftis
 %     destination = [todir, '\Raw_Rest\', subid, '\'];
@@ -36,13 +37,15 @@ for i = 1:numel(sublist)
 %     destination = [todir, '\Preprocessed_Rest\', subid, '\'];
 %     mkdir(destination);
 %     source = fullfile(prepedFuncs(i,1).folder, prepedFuncs(i,1).name);
-%     copyfile(source, destination);
+%     gzip(source, destination);
+%     % copyfile(source, destination);
     
     % copy preprocessed & denoised Rest niftis
     destination = [todir, '\Prep&Denoised_Rest\', subid, '\'];
     mkdir(destination);
     source = fullfile(denoisedFuncs(i,1).folder, denoisedFuncs(i,1).name);
-    copyfile(source, destination);
+    gzip(source, destination);
+    % copyfile(source, destination);
     
     % copy headmotions
     destination = [todir, '\Headmotion\', subid, '\'];
